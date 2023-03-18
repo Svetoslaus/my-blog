212fd91cb3fe26fa536cd6ee008f6e2ac952e350
Svetoslaus <svetoslavshterev@Svetoslavs-Air.fritz.box>
2023-03-18 23:17:54 +0100

    First Commit

---
+++ src/pages/HomePage.js
@@ -0,0 +1,74 @@
+const HomePage = () => {
+
+    return (
+        <>
+        <h1>This is my home page!</h1>
+        <p>
+        Te quoque, Amyclide, posuisset in aethere Phoebus,
+tristia si spatium ponendi fata dedissent;
+qua licet, aeternus tamen es, quotiensque repellit
+ver hiemem Piscique Aries succedit aquoso,
+tu totiens oreris viridique in caespite flores.
+te meus ante omnes genitor dilexit, et orbe
+in medio positi caruerunt praeside Delphi,
+dum deus Eurotan inmunitamque frequentat
+Sparten. nec citharae nec sunt in honore sagittae:
+inmemor ipse sui non retia ferre recusat,
+non tenuisse canes, non per iuga montis iniqui
+ire comes longaque alit adsuetudine flammas.
+iamque fere medius Titan venientis et actae
+noctis erat spatioque pari distabat utrimque:
+corpora veste levant et suco pinguis olivi
+splendescunt latique ineunt certamina disci,
+quem prius aërias libratum Phoebus in auras
+misit et oppositas disiecit pondere nubes;
+        </p>
+        <p>
+        reccidit in solidam longo post tempore terram
+pondus et exhibuit iunctam cum viribus artem.
+protinus inprudens actusque cupidine lusus
+tollere Taenarides orbem properabat, at illum
+dura repercusso subiecit in †aere† tellus
+in vultus, Hyacinthe, tuos. expalluit aeque
+quam puer ipse deus conlapsosque excipit artus
+et modo te refovet, modo tristia vulnera siccat,
+nunc animam admotis fugientem sustinet herbis:
+nil prosunt artes; erat inmedicabile vulnus.
+ut, siquis violas riguoque papaver in horto
+liliaque infringat fulvis horrentia linguis,
+marcida demittant subito caput illa gravatum
+nec se sustineant spectentque cacumine terram,
+sic vultus moriens iacet, et defecta vigore
+ipsa sibi est oneri cervix umeroque recumbit
+‘laberis, Oebalide, prima fraudate iuventa,’
+Phoebus ait ‘videoque tuum, mea crimina, vulnus.
+tu dolor es facinusque meum; mea dextera leto
+inscribenda tuo est. ego sum tibi funeris auctor.
+quae mea culpa tamen? nisi si lusisse vocari
+culpa potest, nisi culpa potest et amasse vocari.
+        </p>
+        <p>
+        atque  utinam pro te vitam tecumque liceret
+reddere! quod quoniam fatali lege tenemur,
+semper eris mecum memorique haerebis in ore.
+te lyra pulsa manu, te carmina nostra sonabunt,
+flosque novus scripto gemitus imitabere nostros.
+tempus et illud erit, quo se fortissimus heros
+addat in hunc florem folioque legatur eodem.’
+talia dum vero memorantur Apollinis ore,
+ecce cruor, qui fusus humo signaverat herbas,
+desinit esse cruor, Tyrioque nitentior ostro
+flos oritur formamque capit quam lilia, si non
+purpureus color his, argenteus esset in illis.
+non satis hoc Phoebo est (is enim fuit auctor honoris):
+ipse suos gemitus foliis inscribit et AI AI
+flos habet inscriptum, funestaque littera ducta est.
+nec genuisse pudet Sparten Hyacinthon, honorque
+durat in hoc aevi, celebrandaque more priorum
+annua praelata redeunt Hyacinthia pompa.
+        </p>
+        </>
+    )
+}
+
+export default HomePage;
---
+++ src/pages/article-content.js
@@ -0,0 +1,35 @@
+const articles = [
+    {
+        name: 'cs50-project',
+        title: 'CS50 Last Project',
+        content: [
+            `Lorem ipsum dolor sit amet, consetetur sadipscing elitr, 
+            sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, 
+            sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. 
+            Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, 
+            sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, 
+            sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. 
+            Stet clita kasd gubergren, 
+            no sea takimata sanctus est Lorem ipsum dolor sit amet.`
+        ]
+    },
+    {
+        name: 'cs50-demo',
+        title: 'CS50 Demo DB',
+        content: [
+            `Lorem ipsum dolor sit amet, consetetur sadipscing elitr, 
+            sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, 
+            sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. 
+            Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, 
+            sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, 
+            sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. 
+            Stet clita kasd gubergren, 
+            no sea takimata sanctus est Lorem ipsum dolor sit amet.`
+        ]
+    }
+
+];
+
+export default articles;
